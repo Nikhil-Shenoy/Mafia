86b7f216518557940be99cca37204352d770f54f
Nikhil Shenoy <nrs76@vi.cs.rutgers.edu>
2015-05-09 23:46:18 -0400

    list should be done

--- structures/player.c
+++ structures/player.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../chat/util.h"
 #include "player.h"
 
 void init_player(Player *newPlayer) {
 	newPlayer->alive = true;
 	newPlayer->saved = false;
-	fd = -1;
+	newPlayer->fd = -1;
 	memset(&(newPlayer->connInfo),'\0',sizeof(newPlayer->connInfo));
 	memset(newPlayer->name,'\0',MAXLINE);
 	memset(newPlayer->role,'\0',MAXLINE);
@@ -23,14 +24,15 @@ void listInsert(char *name, PlayerList *list) {
 
 	if(cur == NULL) 
 		list->head = newPlayer;
-	else
+	else {
 		while(cur->next != NULL)
 			cur = cur->next;
 
 		cur->next = newPlayer;
+	}
 }
 
-void listRemove(char *name, PlayerLlist *list) {
+void listRemove(char *name, PlayerList *list) {
 
 	Player *cur = list->head;
 	Player *prev = list->head;
@@ -63,10 +65,41 @@ Player *listFind(char *name, PlayerList *list) {
 	cur = list->head;
 
 	while(cur != NULL) {
-		
+		if(strcasecmp(name,cur->name) == 0)
+			return cur;
 
+		cur = cur->next;
+	}
 
+	return NULL;
+}
 
+void listPrint(PlayerList *list) {
+
+	Player *cur = list->head;
+	
+	while(cur != NULL) {
+		if(cur->alive)
+			printf("\t%s\n",cur->name);	
+	
+		cur = cur->next;
+	}
+}
+
+void listDestroy(PlayerList *list) {
+
+	Player *cur = list->head;
+	cur = cur->next;
+	Player *prev = list->head;
+	
+	while(cur != NULL) {
+		list->head = cur;
+		prev->next = NULL;
+		free(prev);
+		prev = cur;
+		cur = cur->next;
+	}
+}
 
 
 
--- structures/player.h
+++ structures/player.h
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdbool.h>
 #include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 #include "../chat/util.h"
 
 struct player {
@@ -14,20 +17,22 @@ struct player {
 	struct player *next;
 };
 
-struct playerList PlayerList {
+typedef struct player Player;
+
+struct playerList {
 	Player *head;
 	int size;
 };
 
 
-typedef struct player Player;
 typedef struct playerList PlayerList;
 	
 void init_player(Player *newPlayer);
 
 void listInsert(char *name, PlayerList *list);
 void listRemove(char *name, PlayerList *list);
-Player *listFind(char *name);
+Player *listFind(char *name, PlayerList *list);
 void listPrint(PlayerList *list);
+void listDestroy(PlayerList *list);
 
 
